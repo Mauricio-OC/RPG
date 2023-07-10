478123bd87252465e080e69eb0ce88897e2d1df7
Mauricio-OC <murisc0@hotmail.com>
2023-07-10 17:50:38 -0300

    req 02

--- src/Races/Elf.ts
+++ src/Races/Elf.ts
@@ -1,1 +1,22 @@
 import Race from './Race';
+
+class Elf extends Race {
+  static instances = 0;
+  maxLifePoints: number;
+  
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this.maxLifePoints = 99;
+    Elf.instancesPlus();
+  }
+
+  static instancesPlus(): void {
+    Elf.instances += 1;
+  }
+
+  static createdRacesInstances(): number {
+    return Elf.instances;
+  }
+}
+
+export default Elf;
--- src/Races/Orc.ts
+++ src/Races/Orc.ts
@@ -1,1 +1,22 @@
-import Race from "./Race";
+import Race from './Race';
+
+class Orc extends Race {
+  static instances = 0;
+  maxLifePoints: number;
+  
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this.maxLifePoints = 74;
+    Orc.instancesPlus();
+  }
+
+  static instancesPlus(): void {
+    Orc.instances += 1;
+  }
+
+  static createdRacesInstances(): number {
+    return Orc.instances;
+  }
+}
+
+export default Orc;
